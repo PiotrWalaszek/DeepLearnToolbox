8fe76e836f600687f50782e4c5cfe4c0ca7931e3
Søren Sønderby <tropehjelm@gmail.com>
2013-03-27 13:02:38 +0100

    cast variables before they are copied to gpu

--- util/cpNNtoGPU.m
+++ util/cpNNtoGPU.m
@@ -1,22 +1,33 @@
-function [ dnn ] = cpNNtoGPU( hnn )
+function [ dnn ] = cpNNtoGPU( hnn,cast)
 fld = fields(hnn);
 for i=1:numel(fld)
     fieldName = fld{i};
     switch fieldName
         case 'W'
             for j=1:numel(hnn.W)
-                dnn.W{j} = gpuArray(hnn.W{j});
+                dnn.W{j} = gpuArray(cast(hnn.W{j}));
             end
         case 'vW'
             for j=1:numel(hnn.vW)
-                dnn.vW{j} = gpuArray(hnn.vW{j});
+                dnn.vW{j} = gpuArray(cast(hnn.vW{j}));
             end
         case 'p'
             for j=1:numel(hnn.p)
-                dnn.p{j} = gpuArray(hnn.p{j});
+                dnn.p{j} = gpuArray(cast(hnn.p{j}));
+            end
+        case 'dW'
+            for j=1:numel(hnn.dW)
+                dnn.dW{j} = gpuArray(cast(hnn.dW{j}));
+            end
+        case 'e'
+            for j=1:numel(hnn.e)
+                dnn.e{j} = gpuArray(cast(hnn.e{j}));
+            end
+        case 'a'
+            for j=1:numel(hnn.a)
+                dnn.a{j} = gpuArray(cast(hnn.a{j}));
             end
         otherwise
             dnn.(fieldName) = hnn.(fieldName);
     end
 end
-
