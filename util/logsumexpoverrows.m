6a1de673618477fd1f3f22921f0b47743b45d0fd
Casper Kaae Sønderby <casperkaae@gmail.com>
2013-04-02 11:01:14 +0200

    deleted old logsuoverexps

--- util/logsumexpoverrows.m
+++
@@ -1,6 +0,0 @@
-function res = logsumexpoverrows(a)
-error('not working - use log_sum_over_cols.m')
-maxs_small = max(a,[],1);
-maxs_big = repmat(maxs_small,size(a,1), 1);
-res = log( sum( exp( a-maxs_big), 1)) + maxs_small;
-end
