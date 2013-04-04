dc9497492ef96e1bb16e73727332b995fc12626c
Søren Sønderby <tropehjelm@gmail.com>
2013-04-05 00:09:14 +0200

    clean training

--- tests/test_example_NN_gpu.m
+++ tests/test_example_NN_gpu.m
@@ -15,7 +15,6 @@ test_y  = cast(test_y);
 [train_x, mu, sigma] = zscore(train_x);
 test_x = normalize(test_x, mu, sigma);
 
-%% ex1 vanilla neural net
 rng(0);
 nn = nnsetup([784 1200 1200 1200 10]);
 nn.output = 'softmax';
@@ -39,42 +38,11 @@ opts.plot                   = 1;
 opts.batchsize              = 1000;  %  Take a mean gradient step over this many samples
 opts.ntrainforeval          = 5000; % number of training samples that are copied to the gpu and used to
 opts.outputfolder           = 'nns/hinton';
-% evalute training performance
-% if you have a small dataset set this to number
-% opts.plotfun = @nnplotmatthew;
-% nn.errfun               = @nnmatthew;
-% of samples in your training data
+
 tt = tic;
-[nn_gpu,L,loss] = nntrain_gpu(nn, train_x, train_y, opts);
+[nn_gpu,L,loss] = nntrain(nn, train_x, train_y, opts,test_x,test_y);
 toc(tt);
-%[nn_cpu,L,loss] = nntrain(nn, train_x, train_y, opts);
+
 
 [er_gpu, bad] = nntest(nn_gpu, test_x, test_y);
-%[er_cpu, bad] = nntest(nn_cpu, test_x, test_y);
 fprintf('Error: %f \n',er_gpu);
-%fprintf('Error GPU (single); %f \n',er_cpu);
-
-%
-% load mnist_uint8;
-% cast = @double;
-%
-% train_x = cast(train_x) / 255;
-% test_x  = cast(test_x)  / 255;
-% train_y = cast(train_y);
-% test_y  = cast(test_y);
-%
-% % normalize
-% [train_x, mu, sigma] = zscore(train_x);
-% test_x = normalize(test_x, mu, sigma);
-%
-% %% ex1 vanilla neural net
-% rng(0);
-% nn = nnsetup([784 200 10]);
-%
-% fprintf('DOUBLE PRECISION PERFORMANCE \n')
-% %[nn_gpu,L,loss] = nntrain_gpu(nn, train_x, train_y, opts);
-% [nn_cpu,L,loss] = nntrain(nn, train_x, train_y, opts);
-% %[er_gpu, bad] = nntest(nn_gpu, test_x, test_y);
-% [er_cpu, bad] = nntest(nn_cpu, test_x, test_y);
-% fprintf('Error GPU (double): %f \n',er_gpu);
-% fprintf('Error GPU (double); %f \n',er_cpu);
