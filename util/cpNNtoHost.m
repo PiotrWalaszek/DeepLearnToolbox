e98dbf0dd221dad063fd380124fe2adfdb58dc10
Søren Sønderby <tropehjelm@gmail.com>
2013-04-08 09:03:01 +0200

    cp. dW and db to host. Avoid needing cuda driver to load NN

--- util/cpNNtoHost.m
+++ util/cpNNtoHost.m
@@ -7,7 +7,7 @@ for i=1:numel(fld)
             for j=1:numel(dnn.W)
                 hnn.W{j} = gather(dnn.W{j});
             end
-             case 'b'
+        case 'b'
             for j=1:numel(dnn.b)
                 hnn.b{j} = gather(dnn.b{j});
             end
@@ -15,9 +15,17 @@ for i=1:numel(fld)
             for j=1:numel(dnn.vW)
                 hnn.vW{j} = gather(dnn.vW{j});
             end
-           case 'vb'
+        case 'vb'
             for j=1:numel(dnn.vb)
                 hnn.vb{j} = gather(dnn.vb{j});
+            end
+        case 'dW'
+            for j=1:numel(dnn.dW)
+                hnn.dW{j} = gather(dnn.dW{j});
+            end
+        case 'db'
+            for j=1:numel(dnn.db)
+                hnn.db{j} = gather(dnn.db{j});
             end
         case 'p'
             for j=1:numel(dnn.p)
@@ -31,6 +39,6 @@ for i=1:numel(fld)
             hnn.(fieldName) = 0;
         otherwise
             hnn.(fieldName) = gather(dnn.(fieldName));
-         
+            
     end
 end
