0b5128ce1e776a3663dcb05d19692c66a3180adb
SergiiKashubin <magisteryada@gmail.com>
2013-06-28 15:21:55 +0300

    DBN: Support of more steps of CD added (CDn) RBM training (sampling) modified according to Hinton's guide.

--- DBN/rbmtrain.m
+++ DBN/rbmtrain.m
@@ -1,33 +1,48 @@
 function rbm = rbmtrain(rbm, x, opts)
     assert(isfloat(x), 'x must be a float');
-    m = size(x, 1);
-    numbatches = m / opts.batchsize;
+    if (~isfield(opts,'cdn')), opts.cdn = 1; end;
+    assert(opts.cdn >= 1, 'cdn must be integer >= 1!');
     
-    assert(rem(numbatches, 1) == 0, 'numbatches not integer');
-
+    m = size(x, 1);
+    numbatches = floor(m / opts.batchsize);
+    cdn = opts.cdn;
+    
     for i = 1 : opts.numepochs
         kk = randperm(m);
         err = 0;
         for l = 1 : numbatches
-            batch = x(kk((l - 1) * opts.batchsize + 1 : l * opts.batchsize), :);
+            batch = extractminibatch(kk,l,opts.batchsize,x);
+            batchsize = size(batch,1);  % actual batchsize (last batch may be larger then others)
             
-            v1 = batch;
-            h1 = sigmrnd(repmat(rbm.c', opts.batchsize, 1) + v1 * rbm.W');
-            v2 = sigmrnd(repmat(rbm.b', opts.batchsize, 1) + h1 * rbm.W);
-            h2 = sigm(repmat(rbm.c', opts.batchsize, 1) + v2 * rbm.W');   %dont sample last hidden
+            v = cell(cdn + 1,1);
+            h = cell(cdn + 1,1);
+            h_sample = cell(cdn + 1,1);
+            
+            % always (except last step) sample hidden units
+            % never sample visible units
+            v{1} = batch;
+            h{1} = sigm(repmat(rbm.c', batchsize, 1) + v{1} * rbm.W');
+            h_sample{1} = sample(h{1});
+                        
+            for k = 2 : cdn + 1
+                v{k} = sigm(repmat(rbm.b', batchsize, 1) + h_sample{k-1} * rbm.W);
+                h{k} = sigm(repmat(rbm.c', batchsize, 1) + v{k} * rbm.W');
+                if (k ~= cdn+1), h_sample{k} = sample(h{k}); end;
+            end;
+            
+            % use probabilities, not sampled values, for collecting statistics
+            phase_pos = h{1}' * v{1};
+            phase_neg = h{cdn + 1}' * v{cdn + 1};
 
-            c1 = h1' * v1;
-            c2 = h2' * v2;
-
-            rbm.vW = rbm.momentum * rbm.vW + rbm.alpha * (c1 - c2)     / opts.batchsize;
-            rbm.vb = rbm.momentum * rbm.vb + rbm.alpha * sum(v1 - v2)' / opts.batchsize;
-            rbm.vc = rbm.momentum * rbm.vc + rbm.alpha * sum(h1 - h2)' / opts.batchsize;
+            rbm.vW = rbm.momentum * rbm.vW + rbm.alpha * (phase_pos - phase_neg)     / batchsize;
+            rbm.vb = rbm.momentum * rbm.vb + rbm.alpha * sum(v{1} - v{cdn + 1})' / batchsize;
+            rbm.vc = rbm.momentum * rbm.vc + rbm.alpha * sum(h{1} - h{cdn + 1})' / batchsize;
 
             rbm.W = rbm.W + rbm.vW;
             rbm.b = rbm.b + rbm.vb;
             rbm.c = rbm.c + rbm.vc;
 
-            err = err + sum(sum((v1 - v2) .^ 2)) / opts.batchsize;
+            err = err + sum(sum((v{1} - v{cdn + 1}) .^ 2)) / batchsize;
         end
         
         disp(['epoch ' num2str(i) '/' num2str(opts.numepochs)  '. Average reconstruction error is: ' num2str(err / numbatches)]);
