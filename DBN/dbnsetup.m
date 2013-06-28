4fd362f2012958f2efef9d290a2ad590d4b4855c
SergiiKashubin <magisteryada@gmail.com>
2013-06-28 14:03:18 +0300

    DBN random weights initialization added.

--- DBN/dbnsetup.m
+++ DBN/dbnsetup.m
@@ -1,4 +1,7 @@
 function dbn = dbnsetup(dbn, x, opts)
+
+    rand_weight_sigma = 0.01;
+
     n = size(x, 2);
     dbn.sizes = [n, dbn.sizes];
 
@@ -6,12 +9,15 @@ function dbn = dbnsetup(dbn, x, opts)
         dbn.rbm{u}.alpha    = opts.alpha;
         dbn.rbm{u}.momentum = opts.momentum;
 
-        dbn.rbm{u}.W  = zeros(dbn.sizes(u + 1), dbn.sizes(u));
+        % weights
+        dbn.rbm{u}.W  = normrnd(0, rand_weight_sigma, dbn.sizes(u + 1), dbn.sizes(u));
         dbn.rbm{u}.vW = zeros(dbn.sizes(u + 1), dbn.sizes(u));
 
+        % visible biases
         dbn.rbm{u}.b  = zeros(dbn.sizes(u), 1);
         dbn.rbm{u}.vb = zeros(dbn.sizes(u), 1);
 
+        % hidden biases
         dbn.rbm{u}.c  = zeros(dbn.sizes(u + 1), 1);
         dbn.rbm{u}.vc = zeros(dbn.sizes(u + 1), 1);
     end
