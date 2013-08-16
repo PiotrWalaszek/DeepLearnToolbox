634e6c02e57a53cc54b469e75e600c1547b0314e
Piotr Walaszek <piow89@gmail.com>
2013-08-16 13:40:04 +0200

    Added better info about prediction error in NN

--- DbnForEeg/example.m
+++ DbnForEeg/example.m
@@ -1,16 +1,11 @@
 function example_DBN
 
-load eegP300;
-
-train_x = double(train_x);% / 255;
-test_x  = double(test_x);%  / 255;
-train_y = double(train_y);
-test_y  = double(test_y);
+load eegP300mini;
 
 %% DBN parameters:
 %  if default value is given, parameter may not be set in user code
 
-opts.numepochs  = 1;           % number of epochs (full sweeps through data)
+opts.numepochs  = 70;           % number of epochs (full sweeps through data)
 opts.batchsize  = 10;           % number of traning examples to average gradient over (one mini-batch size)
                                 % (set to size(train_x,1) to perform full-batch learning)
 opts.momentum   = 0.5;          % learning momentum (default: 0)
@@ -23,24 +18,32 @@ opts.hid_units  = 'sigm';       % type of hidden units  (default: 'sigm')
                                 % units can be 'sigm' - sigmoid, 'linear' - linear
                                 % 'NReLU' - noisy rectified linear (Gaussian noise)
 
-dbn.sizes       = [1000 1000 2000];  % size of hidden layers
+dbn.sizes       = [500 500 800];  % size of hidden layers
 
 rng(0);
 
 dbn = dbnsetup(dbn, train_x, opts);
 dbn = dbntrain(dbn, train_x, opts);
 
+save('C:\Users\piow\Documents\DeepLearnToolbox\data\dbn_mini500x500x800.mat','dbn');
+
 %unfold dbn to nn
-nn = dbnunfoldtonn(dbn, 2);
+nn = dbnunfoldtonn(dbn, [150, 2]);
 nn.activation_function = 'sigm';
 
 %train nn
-opts.numepochs =  1;
+opts.numepochs =  100;
 opts.batchsize = 100;
+opts.learningRate_variable = zeros(1,opts.numepochs) + 0.4;
+opts.momentum_variable = zeros(1,opts.numepochs) + 0.9;
+
 nn = nntrain(nn, train_x, train_y, opts);
-[er, bad] = nntest(nn, test_x, test_y);
 
-%assert(er < 0.10, 'Too big error');
-fprintf('error = %f\n',er);
-disp(bad)
-
+error_info = nntest_extended(nn, test_x, test_y);
+fprintf('Matthews correlation coefficient = %f\n',error_info.mcc);
+fprintf('Accurancy = %f\n',error_info.accuracy);
+fprintf('Confusion matrix\nA \tPredicted class\n');
+fprintf('c \t\tNo,\t\tYes\n');
+fprintf('t No\t%d,\t%d\n',error_info.tn,error_info.fp);
+fprintf('u Yes\t%d,\t%d\n',error_info.fn,error_info.tp);
+fprintf('a\nl\n');
---
+++ NN/nntest_extended.m
@@ -0,0 +1,36 @@
+function [info_output] = nntest_extended(nn, x, y)
+    assert(size(y,2) == 2,'Ther is not two class classification');     
+    %Class 1 is negative and class 2 is positive
+    
+    predicted_labels = nnpredict(nn, x);
+    [~, actual_labels] = max(y,[],2);
+    
+    tp = 0; %True positive
+    tn = 0; %True negative
+    fp = 0; %False positive
+    fn = 0; %False negative
+    
+    for i = 1:size(y,1)
+        if actual_labels(i) == 1
+            if predicted_labels(i) == 1
+                tn = tn + 1;
+            else
+                fp = fp + 1;
+            end
+        else %actual lables == 2 
+            if predicted_labels(i) == 2
+                tp = tp + 1;
+            else
+                fn = fn + 1;
+            end
+        end
+    end
+    
+    info_output.tp = tp;
+    info_output.tn = tn;
+    info_output.fp = fp;
+    info_output.fn = fn;
+    info_output.bad = find(predicted_labels ~= actual_labels);    
+    info_output.accuracy = numel(info_output.bad) / size(y, 1);
+    info_output.mcc = (tp*tn-fp*fn)/sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn)); %Matthews correlation coefficient
+end
